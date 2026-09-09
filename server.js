6ec546e70fa516a68de26a380c808918f9783fd3
gdragonstream0818-arch <gdragonstream0818@gmail.com>
2026-09-09 10:19:33 +0900

    Update server.js

--- server.js
+++ server.js
@@ -137,7 +137,7 @@ app.post('/api/capture', async (req, res) => {
   try {
     browser = await puppeteer.launch({
       headless: 'new',
-      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage'],
     });
     const page = await browser.newPage();
     await page.setUserAgent(
